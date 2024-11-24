b037971313376e03809cbe026afc494d9e3e42a3
_Aminya_ <2679588265@qq.com>
2024-11-24 17:48:36 +0800

    Update study.py

--- study.py
+++ study.py
@@ -1,6 +1,8 @@
+#导入外部库
 import time
 import pyautogui
 from pykeyboard import PyKeyboard
+#定义
 keyboard = PyKeyboard()
 text='.pick'
 text2='-like'
@@ -8,15 +10,17 @@ x=input('请输入需要延迟发送的时间：')
 y=input('请输入需要执行的次数：')
 number=int(y)
 count=int(x)
+#延迟模块
 while count>0:
     print(count,'s后开始输入.')
     time.sleep(1)
     count=count - 1
     if count == 0:
         break
+#计数工作模块
 while number > 0:
     keyboard.type_string(text)
-    time.sleep(1)
+    time.sleep(1)                #防止被识别为刷屏操作
     pyautogui.hotkey('enter')
     number=number - 1 
     time.sleep(1.5)
